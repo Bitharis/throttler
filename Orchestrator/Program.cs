b2ce8172730f4e39d1a6c3162ff067fed24a5be9
unknown <bitharis@gmail.com>
2023-02-04 19:55:36 +0100

    init repository

---
+++ Orchestrator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orchestrator
+{
+    internal class Program
+    {
+        static OrchestratorService orchestrator = new OrchestratorService();
+
+        static void Main(string[] args)
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 3; i++)
+            {
+                tasks.Add(Task.Run(TestDriveAsync));
+            }
+
+            Task.WhenAll(tasks).Wait();
+        }
+
+        static async Task TestDriveAsync()
+        {
+            var session = await orchestrator.StartBackup();
+
+            bool gotResult = false;
+
+            while(gotResult == false)
+            {
+                var result = await orchestrator.GetBackupStatus(session);
+                if (result == null)
+                {
+                    await Task.Delay(2000);
+                    continue;
+                }
+                else
+                {
+                    gotResult = true;
+                    Console.WriteLine($"Backup completed. Session: {session}\n\n");
+                }
+            }
+        }
+    }
+}
---
+++ Orchestrator/SomeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchestrator
+{
+    internal class SomeService
+    {
+        public async Task<Result> StartLongRunningTaskAsync(Guid session)
+        {
+            Console.WriteLine($"Executing task for {session}");
+            await Task.Delay(3000);
+            return new Result { Succeeded = true };
+        }
+    }
+}
